3cf19ee63bde7cd482e6be52718ec36193cdb748
duolok <dusan.lecic5@gmail.com>
2024-08-11 13:25:29 +0200

    init: basic struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+
+use std::io::{self, Read};
+use anyhow::{Result, Context};
+use clap::Parser;
+use colored::Colorize;
+
+#[derive(Parser)]
+struct Options {
+
+    #[clap(default_value="Memory overload")]
+    message: String,
+
+    #[clap(short='f', long="file")]
+    /// Add a path to a file
+    elephant_file: Option<std::path::PathBuf>,
+
+    #[clap(short='i', long="stdin")]
+    /// Read the message from STDIN intead of the argument
+    stdin: bool,
+}
+
+
+
+
+fn main() -> Result<()> {
+    let options = Options::parse();
+    let message = options.message;
+
+    println!("{}", &message);
+
+    Ok(())
 }
