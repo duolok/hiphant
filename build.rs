4a6b670fd6f505fac06be7027056637bac4ec177
duolok <dusan.lecic5@gmail.com>
2024-08-12 16:49:07 +0200

    update: using env home

--- build.rs
+++ build.rs
@@ -1,26 +1,27 @@
 use std::env;
 use std::fs;
-use std::path::Path;
+use std::path::{Path};
 
 fn main() {
-    // Define the source directory
-    let src_dir = "animals";
-    
-    // Get the output directory for the build artifacts
+    let home_dir = env::var("HOME").expect("Could not find the home directory");
+
+    let src_dir = Path::new(&home_dir).join(".config/hiphant/animals");
+
+    if !src_dir.exists() {
+        panic!("Source directory {} does not exist", src_dir.display());
+    }
+
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("animals");
 
-    // Create the destination directory if it doesn't exist
     if let Err(e) = fs::create_dir_all(&dest_path) {
         panic!("Failed to create destination directory {}: {}", dest_path.display(), e);
     }
 
-    // Walk the source directory and copy each file
     for entry in fs::read_dir(src_dir).unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
 
-        // Ensure that the entry is a file before attempting to copy
         if path.is_file() {
             let file_name = path.file_name().unwrap();
             let dest_file = dest_path.join(file_name);
@@ -36,3 +37,4 @@ fn main() {
         }
     }
 }
+
